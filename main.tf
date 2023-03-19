1dbd5b744be95c8c70e2b4183fd85c463c2b5c7f
JohnAtGmail <kossjohn@gmail.com>
2023-03-19 12:41:01 -0400

    Requires build command

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
-
 resource "null_resource" "build" {
   triggers = {
     sha1 = sha1(join("", [for f in fileset("${var.root_path}/${var.name}/${var.src_path}", "**") : filesha1("${var.root_path}/${var.name}/${var.src_path}/${f}")]))
   }
 
   provisioner "local-exec" {
-    command     = "npm run build"
+    command     = "npm run ${var.build_cmd}"
     working_dir = "${var.root_path}/${var.name}"
   }
 
--- variables.tf
+++ variables.tf
@@ -8,6 +8,11 @@ variable "root_path" {
   description = "Full path to the root of the project (path to package json)."
 }
 
+variable "build_cmd" {
+  type        = string
+  description = "Name of the package.json build script (e.g. build_dev or build_prod)."
+}
+
 variable "src_path" {
   type        = string
   description = "Relative path (to the root_path) of the source files."
