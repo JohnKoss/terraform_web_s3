8dbdaaea12f5f52fbc29dec67f074ce4948b31a7
JohnAtGmail <kossjohn@gmail.com>
2023-12-18 18:02:59 -0500

    saving

--- main.tf
+++ main.tf
@@ -6,11 +6,6 @@ variable "name" {
   type        = string
   description = "The name of the folder"
 }
-# variable "path" {
-#   type        = string
-#   description = "The path of the folder"
-#   default = "labs"
-# }
 variable "src_path" {
   type        = string
   description = "The location of the files to compile"
@@ -26,38 +21,32 @@ variable "bucket_name" {
   description = "The name of the bucket to hold the files"
 }
 
-///
-# locals {
-#   name           = "lti13/course_navigation"
-#   src_path       = "src"
-#   dist_directory = "dist"
-#   bucket_name    = "lti13.clearbyte.io"
-# }
 /////////////
 locals {
-  sha1 = sha1(join("", [for f in fileset("${path.module}/${var.src_path}", "**") : filesha1("${path.module}/${var.src_path}/${f}")]))
-  sha2 = sha1(join("", [for f in fileset("${path.module}/${var.dist_path}", "**") : filesha1("${path.module}/${var.dist_path}/${f}")]))
+  sha1 = sha1(join("", [for f in fileset("${path.root}/${var.src_path}", "**") : filesha1("${path.root}/${var.src_path}/${f}")]))
+  sha2 = sha1(join("", [for f in fileset("${path.root}/${var.dist_path}", "**") : filesha1("${path.root}/${var.dist_path}/${f}")]))
+  ttt = "${path.root}/${var.dist_path}"
 }
 
 resource "terraform_data" "website" {
   # Defines when the provisioner should be executed
-  triggers_replace = [local.sha1, local.sha2]
+  triggers_replace = [local.sha1, local.sha2,local.ttt]
 
   provisioner "local-exec" {
       command = "npm run build"
-      working_dir = path.module
+      working_dir = path.root
   }
 
-  input = "${local.sha1}${local.sha2}"
+  input = "${local.sha1}${local.sha2}${local.ttt}"
 }
 
 // Upload the HTML, img and javascript files.
 resource "aws_s3_object" "website" {
-  for_each = fileset("${path.module}/${var.dist_path}", "**")
+  for_each = fileset("${path.root}/${var.dist_path}", "**")
 
   bucket = var.bucket_name
   key    = "${var.name}/${each.value}"
-  source = "${path.module}/${var.dist_path}/${each.value}"
+  source = "${path.root}/${var.dist_path}/${each.value}"
 
   content_type = lookup(tomap(local.mime_types), element(split(".", each.key), length(split(".", each.key)) - 1))
 
@@ -82,19 +71,3 @@ locals {
     "txt"  = "text/plain"
   }
 }
-# resource "null_resource" "build" {
-#   triggers = {
-#     sha1 = sha1(join("", [for f in fileset("${var.root_path}/${var.name}/${var.src_path}", "**") : filesha1("${var.root_path}/${var.name}/${var.src_path}/${f}")]))
-#   }
-
-#   provisioner "local-exec" {
-#     command     = "npm run ${var.build_cmd}"
-#     working_dir = "${var.root_path}/${var.name}"
-#   }
-
-#   provisioner "local-exec" {
-#     command     = "aws s3 sync ${var.root_path}/${var.name}/${var.dist_directory} s3://${var.bucket_name}/${var.name}"
-#     working_dir = path.module
-#   }
-
-# }
