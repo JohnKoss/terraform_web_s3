44b5b40c70e5ab5cf5a69046c90481e8e808dc12
JohnAtGmail <kossjohn@gmail.com>
2024-01-01 09:44:07 -0500

    changed paths

--- main.tf
+++ main.tf
@@ -6,25 +6,40 @@ variable "name" {
   type        = string
   description = "The name of the folder"
 }
-variable "src_path" {
-  type        = string
-  description = "The location of the files to compile"
-  default     = "src"
-}
-variable "dist_path" {
-  type        = string
-  description = "The location of the compiled files"
-  default     = "dist"
-}
 variable "bucket_name" {
   type        = string
   description = "The name of the bucket to hold the files"
 }
+variable "path_html_src" {
+  type        = string
+  description = "The location of the files to compile"
+  default     = "../src"
+}
+variable "path_html_dist" {
+  type        = string
+  description = "The location of the compiled files"
+  default     = "../dist"
+}
+variable "path_hcl" {
+  type        = string
+  description = "The location of the lab's configuration files"
+  default     = "../hcl"
+}
+variable "path_scoring" {
+  type        = string
+  description = "The location of the lab's scoresheet and related files (if any)"
+  default     = "../scoring"
+}
+variable "path_objects" {
+  type        = string
+  description = "The location of any object files the lab may require the learner to use."
+  default     = "../objects"
+}
 
 /////////////
 locals {
-  sha1 = sha1(join("", [for f in fileset("${path.root}/${var.src_path}", "**") : filesha1("${path.root}/${var.src_path}/${f}")]))
-  sha2 = sha1(join("", [for f in fileset("${path.root}/${var.dist_path}", "**") : filesha1("${path.root}/${var.dist_path}/${f}")]))
+  sha1 = sha1(join("", [for f in fileset("${path.root}/${var.path_html_src}", "**") : filesha1("${path.root}/${var.path_html_src}/${f}")]))
+  sha2 = sha1(join("", [for f in fileset("${path.root}/${var.path_html_dist}", "**") : filesha1("${path.root}/${var.path_html_dist}/${f}")]))
 }
 
 resource "terraform_data" "website" {
@@ -41,11 +56,11 @@ resource "terraform_data" "website" {
 
 // Upload the HTML, img and javascript files.
 resource "aws_s3_object" "website" {
-  for_each = fileset("${path.root}/${var.dist_path}", "**")
+  for_each = fileset("${path.root}/${var.path_html_dist}", "**")
 
   bucket = var.bucket_name
   key    = "${var.name}/html/${each.value}"
-  source = "${path.root}/${var.dist_path}/${each.value}"
+  source = "${path.root}/${var.path_html_dist}/${each.value}"
 
   content_type = lookup(tomap(local.mime_types), element(split(".", each.key), length(split(".", each.key)) - 1))
 
@@ -74,25 +89,25 @@ locals {
 ##########
 
 resource "aws_s3_object" "hcl" {
-  for_each = fileset("${path.root}/hcl", "**")
+  for_each = fileset("${var.path_hcl}", "**")
 
   bucket = var.bucket_name
   key    = "${var.name}/hcl/${each.value}"
-  source = "${path.root}/hcl/${each.value}"
+  source = "${var.path_hcl}/${each.value}"
 }
 
 resource "aws_s3_object" "scoring" {
-  for_each = fileset("${path.root}/scoring", "**")
+  for_each = fileset("${var.path_scoring}", "**")
 
   bucket = var.bucket_name
   key    = "${var.name}/scoring/${each.value}"
-  source = "${path.root}/scoring/${each.value}"
+  source = "${var.path_scoring}/${each.value}"
 }
 
 resource "aws_s3_object" "objects" {
-  for_each = fileset("${path.root}/objects", "**")
+  for_each = fileset("${var.path_objects}", "**")
 
   bucket = var.bucket_name
   key    = "${var.name}/objects/${each.value}"
-  source = "${path.root}/objects/${each.value}"
+  source = "${var.path_objects}/${each.value}"
 }
